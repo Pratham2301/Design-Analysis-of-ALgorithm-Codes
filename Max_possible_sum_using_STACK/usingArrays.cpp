450773c34e299fd9e032fc901ea92a5eb8552f1c
Pratham2301 <prathamrajbhoj2003@gmail.com>
2022-09-04 11:02:48 +0530

    adding both codes

--- Max_possible_sum_using_STACK/usingArrays.cpp
+++ Max_possible_sum_using_STACK/usingArrays.cpp
@@ -1,99 +1,129 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define pb push_back
+void display_stack(int *, int);
+void push_stack(int *, int *, int);
+void pop_stack(int *, int *);
+int stack_empty(int);
+int stack_full(int *, int);
+int top_stack(int *, int);
+
+/////////////////////////////////////////* MAIN FUNCTION BEEGINS *////////////////////////////////////////////////
 
 void solve()
 {
-    stack<int> st1;
-    stack<int> st2;
-    stack<int> st3;
+    int *stack1;
+    int *stack2;
+    int *stack3;
 
-    int n1, n2, n3;
+    stack1 = new int();
+    stack2 = new int();
+    stack3 = new int();
+
+    int size1, size2, size3;
     int sum1 = 0, sum2 = 0, sum3 = 0;
+    int index1 = -1, index2 = -1, index3 = -1;
 
     cout << "\nEnter Size of 1st Stack : ";
-    cin >> n1;
+    cin >> size1;
 
     cout << "\nEnter values to be pushed in Stack 1 : ";
 
-    for (int i = 0; i < n1; i++)
+    for (int i = 0; i < size1; i++)
     {
         int x;
         cin >> x;
         sum1 += x;
-        st1.push(x);
+        push_stack(stack1, &index1, x);
     }
 
     cout << "\nEnter Size of 2nd stack : ";
-    cin >> n2;
+    cin >> size2;
 
     cout << "\nEnter values to be pushed in Stack 2 : ";
 
-    for (int i = 0; i < n2; i++)
+    for (int i = 0; i < size2; i++)
     {
         int x;
         cin >> x;
         sum2 += x;
-        st2.push(x);
+        push_stack(stack2, &index2, x);
     }
 
     cout << "\nEnter Size of 3rd stack : ";
-    cin >> n3;
+    cin >> size3;
 
     cout << "\nEnter values to be pushed in Stack 3 : ";
 
-    for (int i = 0; i < n3; i++)
+    for (int i = 0; i < size3; i++)
     {
         int x;
         cin >> x;
         sum3 += x;
-        st3.push(x);
+        push_stack(stack3, &index3, x);
     }
 
-    // cout<<sum1<<" "<<sum2<<" "<<sum3<<endl;
+    // cout<<endl<<index1<<" "<<index2<<" "<<index3<<endl;
+    // cout<<endl<<sum1<<" "<<sum2<<" "<<sum3<<endl;
+
+    // display_stack(stack1, index1);
+    // display_stack(stack2, index2);
+    // display_stack(stack3, index3);
+
+    // cout<<endl<<top_stack(stack1, index1);
+    // cout<<endl<<top_stack(stack2, index2);
+    // cout<<endl<<top_stack(stack3, index3);
+
+    // pop_stack(stack1,&index1);
+    // pop_stack(stack2,&index2);
+    // pop_stack(stack3,&index3);
+
+    // cout<<endl<<top_stack(stack1, index1);
+    // cout<<endl<<top_stack(stack2, index2);
+    // cout<<endl<<top_stack(stack3, index3);
 
     while (true)
     {
         if (sum1 >= sum2 && sum1 >= sum3)
         {
 
-            sum1 -= st1.top();
-            st1.pop();
+            sum1 -= top_stack(stack1, index1);
+            pop_stack(stack1, &index1);
         }
         else if (sum2 >= sum1 && sum2 >= sum3)
         {
-            sum2 -= st2.top();
-            st2.pop();
+            sum2 -= top_stack(stack2, index2);
+            pop_stack(stack2, &index2);
         }
         else if (sum3 >= sum1 && sum3 >= sum2)
         {
-            sum3 -= st3.top();
-            st3.pop();
+            sum3 -= top_stack(stack3, index3);
+            pop_stack(stack3, &index3);
         }
 
         if (sum1 == sum2 && sum2 == sum3)
         {
-            cout << "\n\nMAXIMUM SUM IS POSSIBLE" << endl;
-            cout << "\nSUM IS : " << sum1 << endl;
+            cout << endl << endl << "MAXIMUM SUM IS POSSIBLE" << endl;
+            cout << "SUM IS : " << sum1 << endl;
             return;
         }
 
         if (sum1 == 0 || sum2 == 0 || sum3 == 0)
         {
-            cout << "\n\nMAX SUM IS 0\n" << endl;
+            cout << endl << endl << "MAX SUM IS 0" << endl;
             return;
         }
     }
+
+    return;
 }
 
 int main()
 {
-    ll tc = 1;
+    int tc = 1;
     // cin >> tc;
 
-    for (ll i = 1; i <= tc; i++)
+    for (int i = 1; i <= tc; i++)
     {
         // cout<<"Case #"<<i<<": ";
         solve();
@@ -101,3 +131,52 @@ int main()
 
     return 0;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void push_stack(int stack[], int *top_s, int item)
+{
+    *top_s = *top_s + 1;
+    stack[*top_s] = item;
+    // cout<<"\nindex : "<<*top_s<<" -- value : "<<item;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void pop_stack(int stack[], int *top_s)
+{
+    stack[*top_s] = 0;
+    *top_s = *top_s - 1;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+int top_stack(int *stack, int top_s)
+{
+    if (top_s < 0)
+        return -1;
+    return stack[top_s];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void display_stack(int arr[], int top)
+{
+    // cout<<"topsize : "<<top<<endl;
+
+    cout << "STACK ELEMENTS ARE : \n";
+    for (int i = 0; i <= top; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl
+         << endl;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
